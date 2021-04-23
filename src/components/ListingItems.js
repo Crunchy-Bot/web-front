230545bf2e75fc26c04dca159b4fadd8ad51291a
chillfish8 <hburt2003@gmail.com>
2021-04-23 11:31:15 +0100

    update user panel

--- src/components/ListingItems.js
+++ src/components/ListingItems.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 let counter = 0;
 
-function IconSelection(props) {
-    let icon = props.icon;
-    let link = props.url;
-    let msg = props.msg;
-    let desc = props.desc;
-    let title = props.title;
-    let author = props.author;
-    let hasViewed = props.viewed;
-
+function RecommendedItem(props) {
+    let data = props.data;
+    let icon = data.icon;
+    let link = data.url;
+    let msg = data.msg;
+    let desc = data.desc;
+    let title = data.title;
+    let author = data.author;
+    let hasViewed = data.viewed;
 
     const [toggled, setToggle] = useState(false);
 
@@ -24,7 +24,7 @@ function IconSelection(props) {
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                     </button>
-                    <button className="text-crunchy w-6 h-6">
+                    <div className="text-crunchy w-6 h-6">
                         { hasViewed ? (
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                               <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
@@ -36,7 +36,7 @@ function IconSelection(props) {
                             </svg>
                         )}
 
-                    </button>
+                    </div>
                     <h1 className="text-gray-500 font-semibold mx-4">{ author }</h1>
                     { link !== null ? (
                         <Link to={link} className="text-blue-400 hover:text-blue-500 cursor-pointer font-semibold text-lg">
@@ -83,23 +83,168 @@ function IconSelection(props) {
     )
 }
 
+
+function WatchlistItem(props) {
+    let data = props.data;
+    let icon = data.icon;
+    let link = data.url;
+    let desc = data.desc;
+    let title = data.title;
+
+    const [toggled, setToggle] = useState(false);
+    const [bookmarked, bookmark] = useState(false);
+
+    return (
+        <div className="flex flex-col bg-discord-dark shadow-small-sharp rounded w-2/3 mx-4 my-2">
+            <div className="flex justify-between items-center h-16">
+                <div className="flex items-center space-x-4 mx-4">
+                    <button className="text-red-700 w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </button>
+                    <button
+                        onClick={() => bookmark(!bookmarked)}
+                        className="outline-none focus:outline-none text-crunchy w-6 h-6">
+                        { bookmarked ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                              <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
+                            </svg>
+                        ) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+                            </svg>
+                        )}
+                    </button>
+                    { link !== null ? (
+                        <Link to={link} className="text-blue-400 hover:text-blue-500 cursor-pointer font-semibold text-lg">
+                            { title }
+                        </Link>
+                    ) : (
+                        <h1 className="text-gray-300 cursor-default font-semibold text-lg">
+                            { title }
+                        </h1>
+                    ) }
+
+                </div>
+                <div className="mx-4">
+                    <button
+                        className={`transform ${toggled ? "-rotate-90" : "rotate-0"} transition duration-200 outline-none focus:outline-none text-white h-6 w-6`}
+                        onClick={() => setToggle(!toggled)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div className={`${!toggled ? "hidden": "flex"} h-64`}>
+                <div className="flex items-center mx-4">
+                    <img className="w-40 object-contain rounded shadow-small-sharp" src={icon} alt=""/>
+                    <div className="flex flex-col w-full h-full mx-4 py-4">
+                        <div className="flex pb-3">
+                            <h1 className="text-gray-100 font-semibold">Description</h1>
+                        </div>
+                        <p className="text-gray-400 text-lg border-l-2 border-crunchy w-full px-2">
+                            {desc}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+function FavouritesItem(props) {
+    let data = props.data;
+    let icon = data.icon;
+    let link = data.url;
+    let desc = data.desc;
+    let title = data.title;
+
+    const [toggled, setToggle] = useState(false);
+    const [bookmarked, bookmark] = useState(false);
+
+    return (
+        <div className="flex flex-col bg-discord-dark shadow-small-sharp rounded w-2/3 mx-4 my-2">
+            <div className="flex justify-between items-center h-16">
+                <div className="flex items-center space-x-4 mx-4">
+                    <button className="text-red-700 w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </button>
+                    <button
+                        onClick={() => bookmark(!bookmarked)}
+                        className="outline-none focus:outline-none text-crunchy w-6 h-6">
+                        { bookmarked ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                              <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
+                            </svg>
+                        ) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+                            </svg>
+                        )}
+                    </button>
+                    { link !== null ? (
+                        <Link to={link} className="text-blue-400 hover:text-blue-500 cursor-pointer font-semibold text-lg">
+                            { title }
+                        </Link>
+                    ) : (
+                        <h1 className="text-gray-300 cursor-default font-semibold text-lg">
+                            { title }
+                        </h1>
+                    ) }
+
+                </div>
+                <div className="mx-4">
+                    <button
+                        className={`transform ${toggled ? "-rotate-90" : "rotate-0"} transition duration-200 outline-none focus:outline-none text-white h-6 w-6`}
+                        onClick={() => setToggle(!toggled)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div className={`${!toggled ? "hidden": "flex"} h-64`}>
+                <div className="flex items-center mx-4">
+                    <img className="w-40 object-contain rounded shadow-small-sharp" src={icon} alt=""/>
+                    <div className="flex flex-col w-full h-full mx-4 py-4">
+                        <div className="flex pb-3">
+                            <h1 className="text-gray-100 font-semibold">Description</h1>
+                        </div>
+                        <p className="text-gray-400 text-lg border-l-2 border-crunchy w-full px-2">
+                            {desc}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 function ListItems(props) {
     let entities = props.entities;
+    let type = props.typeOfList;
 
     let rendered = [];
     for (let entity of entities) {
-        rendered.push(
-            <IconSelection
-                key={counter++}
-                icon={entity.icon}
-                msg={entity.msg}
-                url={entity.url}
-                viewed={entity.viewed}
-                author={entity.author}
-                title={entity.title}
-                desc={entity.desc}
-            />
-        )
+        let item;
+        switch (type) {
+            case "favourites":
+                item = <FavouritesItem key={counter++} data={entity}/>
+                break;
+            case "watchlist":
+                item = <WatchlistItem key={counter++} data={entity}/>
+                break;
+            case "recommended":
+                item = <RecommendedItem key={counter++} data={entity}/>
+                break;
+        }
+
+        rendered.push(item)
     }
 
     return (
--- src/pages/User.js
+++ src/pages/User.js
@@ -2,11 +2,40 @@ import React from 'react';
 
 import Footer from "../components/Footer";
 import ListItems from "../components/ListingItems";
+import { Link, useLocation } from "react-router-dom";
+import {commandsInfo} from "../data/CommandInfo";
 
 const iconArea = process.env.PUBLIC_URL;
 
+const validTargets = {
+    "favourites": 1,
+    "watchlist": 2,
+    "recommended": 3,
+    "settings": 4,
+}
+
 function User(props) {
+    const loc = useLocation();
+    let maybeTarget = new URLSearchParams(loc.search).get('target');
+
+    let render;
+    if (maybeTarget === null  || maybeTarget === "favourites") {
+        maybeTarget = "favourites";
+        render = <div></div>
+    } else {
+        let details = validTargets[maybeTarget];
+        if (details === null) {
+            maybeTarget = "favourites";
+            render = <div></div>
+        } else {
+            render = <div></div>
+        }
+    }
+
     let id = props.id;
+
+    const activeTabCss = "flex items-center text-white text-lg font-semibold mx-4 border-b-2 border-crunchy";
+    const defaultTabCss = "flex items-center text-white text-lg font-semibold mx-4 border-b-2 border-transparent";
 
     let example = {
         title: "Osamake: Romcom Where The Childhood Friend Won't Lose",
@@ -27,62 +56,111 @@ function User(props) {
         viewed: true,
     };
 
+    let user = {
+        id: '14923423752311434224',
+        name: 'ChillFish8'
+    }
+
     return (
         <div className="flex flex-col justify-between bg-gradient-about min-h-screen">
-            <div className="flex flex-col items-center w-full px-8">
-                <div className="flex justify-between h-64 w-11/12">
-                    <div className="flex w-3/4">
-                        <img
-                            className="self-center h-48 h-48 object-contain rounded-full shadow-small-sharp"
-                            src={`${iconArea}/cf-512.webp`}
-                            alt=""
-                            loading="lazy"
-                        />
-                        <div className="flex flex-col space-y-4 px-8 py-4 w-full">
-                            <div className="flex">
-                                <h1 className="text-white text-2xl font-bold border-b-2 border-crunchy">
-                                    ChillFish8
-                                </h1>
-                            </div>
-                            <h1 className="text-gray-100 text-lg font-semibold">Bio</h1>
-                            <p className="text-gray-400 text-lg px-4 py-2 my-4 border-l-2 border-gray-800 w-full">
-                                Life is hard.
-                            </p>
-                        </div>
+            <div className="grid grid-cols-4 grid-rows-1 gap-4 w-full px-8 z-20">
+                <div className="flex flex-col items-center col-span-1">
+                    <img
+                        className="self-center h-64 h-64 object-contain rounded-full shadow-small-sharp"
+                        src={`${iconArea}/cf-512.webp`}
+                        alt=""
+                        loading="lazy"
+                    />
+
+                    <div className="flex flex-col py-4 w-64">
+                        <h1 className="text-white text-2xl font-bold">
+                            { user.name }
+                        </h1>
+                        <h1 className="text-gray-500">
+                            { user.id }
+                        </h1>
                     </div>
 
                     <div className="flex flex-col text-white text-lg space-y-1 py-4">
-                        <div className="flex pb-3">
-                            <h1 className="text-2xl text-white border-b-2 border-crunchy">User Stats</h1>
-                        </div>
-                        <h1>
+                        <h1 className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-4 transform rotate-90" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+                            </svg>
                             <code className="text-crunchy pr-1">32</code>
                             Recommendations to
                             <code className="text-crunchy px-1">3</code>
                             users
                         </h1>
-                        <h1>
+                        <h1 className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                            </svg>
                             <code className="text-crunchy pr-1">32</code>
                             Items in recommended
                         </h1>
-                        <h1>
+                        <h1 className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                            </svg>
                             <code className="text-crunchy pr-1">12</code>
                             Items in watchlist
                         </h1>
-                        <h1>
+                        <h1 className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                            </svg>
                             <code className="text-crunchy pr-1">4</code>
                             Items in favourites
                         </h1>
 
                     </div>
                 </div>
-                <div className="flex flex-col w-11/12 mt-12 px-4">
+                <div className="flex flex-col col-span-3 mt-12">
                     <div className="flex mx-2 mb-2">
-                        <button className="text-white text-lg font-semibold mx-4 ">Favourites</button>
-                        <button className="text-white text-lg font-semibold mx-4">Watchlist</button>
-                        <button className="text-white text-lg font-semibold mx-4 border-b-2 border-crunchy">Recommended</button>
+                        <Link
+                            to="?target=favourites"
+                            className={maybeTarget === "favourites" ? activeTabCss : defaultTabCss}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                            </svg>
+                            Favourites
+                        </Link>
+                        <Link
+                            to="?target=watchlist"
+                            className={maybeTarget === "watchlist" ? activeTabCss : defaultTabCss}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                            </svg>
+                            Watchlist
+                        </Link>
+                        <Link
+                            to="?target=recommended"
+                            className={maybeTarget === "recommended" ? activeTabCss : defaultTabCss}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                            </svg>
+                            Recommended
+                        </Link>
+                        <Link
+                            to="?target=settings"
+                            className={maybeTarget === "settings" ? activeTabCss : defaultTabCss}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+                            </svg>
+                            Settings
+                        </Link>
                     </div>
-                   <ListItems entities={[example, example2]}/>
+                    { maybeTarget !== "settings" ? (
+                        <ListItems entities={[example, example2]} typeOfList={maybeTarget}/>
+                    ) : (
+                        <h1>Hey yes</h1>
+                    ) }
                 </div>
             </div>
             <Footer/>
